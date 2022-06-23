25de483377db4b56bad9d0e6ad4cd9123bd6d4f0
Aimee Sharp <sharpaim23@gmail.com>
2022-06-23 09:25:31 -0500

    added endpoint in server.js

--- server.js
+++ server.js
@@ -17,12 +17,22 @@ MongoClient.connect(dbConnectionStr)
     collection = db.collection('movies')
   })
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(cors())
+  
+  app.set('view engine', 'ejs')
+  app.use(express.static('public'))
+  app.use(express.urlencoded({extended: true}))
+  app.use(express.json())
+  app.use(cors())
+  
+  app.get('/', async (req,res) => {
+    try{
+      res.render('index.ejs')
 
-app.listen(process.env.PORT || PORT, () =>{
+    }catch(error){
+      res.status(500).send({message: error.message});
+    }
+  })
+  
+  app.listen(process.env.PORT || PORT, () =>{
     console.log(`Server is listening on PORT`);
 })
